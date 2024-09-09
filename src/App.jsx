4af19bf00a4a89a69ce7297a286d63a7d3f594f6
MiraProfissional <jvitor.gmira@gmail.com>
2024-09-09 19:38:34 -0300

    :sparkles: feat(header): development of the feature Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import './App.css';
+import Logo from './components/Logo';
+import profile from './images/perfil.svg'
+import shoppingBage from './images/sacola.svg'
+
+const textOptions = ['CATEGORIAS','FAVORITOS','MINHA ESTANTE']
+const icons = [profile,shoppingBage]
 
 function App() {
   return (
-    <div>
-      <h1>Teste</h1>
+    <div className='App'>
+      <header className='App-header'>
+        <Logo />
+        <ul className='options'>
+          {textOptions.map((text) => (
+            <li className='option'><p>{text}</p></li>
+          ))}
+        </ul>
+        <ul className='icons'>
+          {icons.map((icons) => (
+            <li className='icons-navbar'><img src={icons} alt="" /></li>
+          ))}
+        </ul>
+      </header>
     </div>
   );
 }
---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from '../../images/logo.svg'
+import './styles.css'
+
+function Logo() {
+  return (
+    <div className='logo'>
+      <img src={logo} alt="logo" className='logo-img'/>
+    <p><strong>Alura</strong>Books</p>
+  </div>
+  )
+}
+
+export default Logo
