b8c902e2c54d562bb99b81dc77e72c32eb3b9da9
MiraProfissional <jvitor.gmira@gmail.com>
2024-09-18 10:32:39 -0300

    :heavy_plus_sign: feat: adding React Router DOM to the project

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { createGlobalStyle } from 'styled-components';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -28,6 +29,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/favoritos" element={<p>Oi!</p>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
